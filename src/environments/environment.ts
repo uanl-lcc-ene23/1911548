6726fc1ac739aab5c86d53a0be82100691b42b66
HiramMcc <123848347+HiramMcc@users.noreply.github.com>
2023-04-26 00:29:43 -0600

    PIA-SFOTIPY

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'miapp-1911548',
+    appId: '1:163791116729:web:b24f3ee6242b55de9bbe68',
+    storageBucket: 'miapp-1911548.appspot.com',
+    apiKey: 'AIzaSyBgNn080-dYqD2RAdQHNpOsXYuUNwAsKfw',
+    authDomain: 'miapp-1911548.firebaseapp.com',
+    messagingSenderId: '163791116729',
+  },};
